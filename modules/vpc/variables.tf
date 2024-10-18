d4e76110c1207ef9189585c2c68e4885f5ab8250
Dharmendrasingh9760 <152598648+Dharmendrasingh9760@users.noreply.github.com>
2024-10-18 12:36:48 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  description = "The AWS region to deploy the VPC"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "create_nat_gateway" {
+  description = "Flag to create a NAT Gateway"
+  type        = bool
+  default     = true
+}
